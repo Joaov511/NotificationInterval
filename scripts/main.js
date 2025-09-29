c6d116bdd7725e4b6d5aae2cc5d4fb69efa9482b
João Vitor Von Ah <jv232479@gmail.com>
2025-09-29 20:13:05 -0300

    add input to choose text for notification

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,38 @@
 import { sendNotification, closePreviousNotification } from "./notifications.js";
 import { startButtonStyle, cancelButtonStyle } from "./startButton.js";
 
-const timeInput = document.getElementById("timeInput");
+const minuteInput = document.getElementById("minuteInput");
 const startBtn = document.getElementById("startBtn");
 const intervalSetWarning = document.getElementById("intervalSet");
+const labelInput = document.getElementById("labelInput");
 
 const minute = 1000*60;
 let isNotificationsActive = false;
-let notificationText = "Feche os chamados";
 const standardInputColor = "white";
 const lockedInputColor = "rgba(204, 206, 209, 1)";
+let notificationText;
 let activeInterval;
 
 function activateNotifications(intervalTime) {
+    notificationText = labelInput.value;
     sendNotification(notificationText);
-    intervalSetWarning.textContent = `Interval set for each ${intervalTime} minutes`;
+    intervalSetWarning.textContent = `Interval set for each ${intervalTime} minute(s)`;
     cancelButtonStyle(startBtn);
-    timeInput.readOnly = true;
-    timeInput.style.backgroundColor = lockedInputColor;
+    minuteInput.readOnly = true;
+    minuteInput.style.backgroundColor = lockedInputColor;
 }
 
 function cancelNotifications() {
     closePreviousNotification();
     intervalSetWarning.textContent = "";
     startButtonStyle(startBtn);
-    timeInput.readOnly = false;
-    timeInput.style.backgroundColor = standardInputColor;
+    minuteInput.readOnly = false;
+    minuteInput.style.backgroundColor = standardInputColor;
 }
 
 
 startBtn.addEventListener("click", () => {
-    let intervalTime = Number(timeInput.value);
+    let intervalTime = Number(minuteInput.value);
     if(!isNotificationsActive) {
         activateNotifications(intervalTime);
         activeInterval = setInterval(() => {
